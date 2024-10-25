92a5854f237576128ea9aa43ba228952481d783e
Carlos <carloskirby01@gmail.com>
2024-10-24 22:45:09 -0600

    Tutorial

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from state_manager import StateManager
 from main_menu import MainMenu
 from lv_selector import LevelSelector
 from lv_1 import Level1
-
+from tutorial import Tutorial
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
     
     state_manager = StateManager()
+    state_manager.add_state("Tutorial", Tutorial(state_manager)) #Agrege el tutorial al state manager
     state_manager.add_state("main_menu", MainMenu(state_manager))
     state_manager.add_state("levels", LevelSelector(state_manager))
     state_manager.add_state("level1", Level1(state_manager))
