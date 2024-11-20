58ffcaa00b987f9bffb0cc324a503ad2ce53e729
iNoxy10 <erickgerman170106@gmail.com>
2024-11-20 12:35:42 -0600

    Arreglo en controls

--- controls.py
+++ controls.py
@@ -9,24 +9,30 @@ class ControlsScreen:
         # Datos de pantalla
         self.state_manager = state_manager
         self.selected_level = None
-        self.screen = pygame.display.set_mode((1280, 720)) # Creamos la ventana con sus medidas
+        self.screen = pygame.display.set_mode((1245, 720)) # Creamos la ventana con sus medidas
         self.clock = pygame.time.Clock() # Reloj para controlar los FPS
         
         # Carga de recursos
-        self.background = pygame.image.load("Manual.jpg")
+        self.background = pygame.image.load("assets/sprites/TUTORIAL.png")
         font_game = pygame.font.Font("assets/fonts/GAME.TTF", 27)
-        self.back_image = pygame.image.load("assets/sprites/BOTONCONTINUAR.png")
+        self.back_image = pygame.image.load("assets/sprites/BOTONSIGUIENTE.png")
+
         #Textos
     
         self.howtoplay_text = font_game.render(localization.get_text("how_to_play"), True, (0, 0, 0))  # Negro
         
-        
+        self.back_image = pygame.transform.scale(self.back_image, (110, 110))
 
         # Escalar los recursos y efecto espejo
         self.back_image = pygame.transform.flip(self.back_image, True, False)
+
+        
         
         # Crear botones
-        self.back_button = Button(self.back_image, (100, 650), "", self.get_font(20), "Black", "Green")
+        self.back_button = Button(self.back_image, (170, 630), "", self.get_font(20), "Black", "Green")
+
+        #Escalar
+        
         
         # Carga de texto
         self.font = self.get_font(1)
